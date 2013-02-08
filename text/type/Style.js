c6241eb28e97b49025904c12bf16adb162359acd
Tony Findeisen <github@it-ony.de>
2013-02-08 16:31:48 +0100

    removed default Style

--- text/type/Style.js
+++ text/type/Style.js
@@ -2,16 +2,6 @@ define(["js/core/Bindable", "underscore"], function(Bindable, _) {
 
     return Bindable.inherit("text.type.Style", {
 
-        // always use getter to access these values, because the can be calculated
-
-        defaults: {
-            fontFamily: "Arial",
-            fontWeight: "normal",
-            fontStyle: "normal",
-            fontSize: "12pt",
-            color: "black"
-        },
-
         compose: function() {
             return _.clone(this.$);
         }
