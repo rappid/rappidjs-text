36887b575c8c745744b8b71e826b35c85c2fe19b
Tony Findeisen <github@it-ony.de>
2013-02-08 15:40:04 +0100

    compose for style

--- text/type/Style.js
+++ text/type/Style.js
@@ -7,29 +7,13 @@ define(["js/core/Bindable", "underscore"], function(Bindable, _) {
         defaults: {
             fontFamily: "Arial",
             fontWeight: "normal",
-            fontHeight: "normal",
+            fontStyle: "normal",
             fontSize: "12pt",
             color: "black"
         },
 
-        getFontFamily: function() {
-            return this.$.fontFamily;
-        },
-
-        getFontWeight: function() {
-            return this.$.fontWeight;
-        },
-
-        getFontHeight: function() {
-            return this.$.fontHeight;
-        },
-
-        getFontSize: function() {
-            return this.$.fontSize;
-        },
-
-        getColor: function() {
-            return this.$.color;
+        compose: function() {
+            return _.clone(this.$);
         }
 
     });
