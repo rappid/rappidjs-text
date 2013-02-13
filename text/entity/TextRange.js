0250165158f9e5b0ec351d94880ac5a39997c587
Marcus Krejpowicz <krebbl@gmail.com>
2013-02-13 14:53:05 +0100

    fixed setting of anchorIndex

--- text/entity/TextRange.js
+++ text/entity/TextRange.js
@@ -5,8 +5,8 @@ define(["js/core/Bindable"], function (Bindable) {
         _commitChangedAttributes: function ($) {
             this.callBase($);
             if (this._hasSome($, ["anchorIndex", "activeIndex"])) {
-                if ((this.$.activeIndex === null || this.$.activeIndex === -1)) {
-                    this.$.activeIndex = this.$.anchorIndex;
+                if ((this.$.anchorIndex === null || this.$.anchorIndex === -1)) {
+                    this.$.anchorIndex = this.$.activeIndex;
                 }
                 if (this.$.anchorIndex < this.$.activeIndex) {
                     this.set({
