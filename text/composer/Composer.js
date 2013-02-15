3bf33dd78fdfcc89f16cbd74cad2bee2d3e89e04
Marcus Krejpowicz <krebbl@gmail.com>
2013-02-15 14:31:52 +0100

    added Composer

--- text/composer/Composer.js
+++ text/composer/Composer.js
@@ -1,11 +1,36 @@
-define(["js/core/Base"], function (Base) {
+define(["js/core/Base","text/entity/Layout", "text/entity/FlowGroupElement", "text/entity/FlowLeafElement"], function (Base, Layout, FlowGroupElement, FlowLeafElement) {
 
 
     return Base.inherit('text.composer.Composer', {
 
+        ctor: function(layout, measuring){
+            this.$layout = layout || new Layout();
+            this.$measuring = measuring;
+
+            if(!this.$measuring){
+                throw new Error("No Measuring defined for Composer");
+            }
+
+            this.callBase();
+        },
 
         compose: function (textFlow) {
+            var currentLine = 1;
 
+            function composeLeaf(element){
+
+
+            }
+
+            function composeGroup(element){
+                if(element instanceof FlowGroupElement){
+                    element.$.children.each(composeGroup);
+                } else if(element instanceof FlowLeafElement){
+                    composeLeaf(element);
+                }
+            }
+
+            composeGroup(textFlow);
         }
 
 
