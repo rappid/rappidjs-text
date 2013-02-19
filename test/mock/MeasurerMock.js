7650cfb145bdc775e4552a3a9d88bd10e719e377
Tony Findeisen <github@it-ony.de>
2013-02-19 17:20:26 +0100

    implemented first MeasureMock and ComposerTest

---
+++ test/mock/MeasurerMock.js
@@ -0,0 +1,18 @@
+define(["text/composer/Measurer", "text/metric/Metric"], function (Measurer, Metric) {
+
+    return Measurer.inherit("test.mock.MeasurerMock", {
+
+        measureText: function(span) {
+
+            var style = span.composeStyle(),
+                fontSize = style.fontSize || 0;
+
+            return new Metric({
+                width: span.textLength() * fontSize,
+                height: fontSize
+            });
+        }
+
+    });
+
+});
