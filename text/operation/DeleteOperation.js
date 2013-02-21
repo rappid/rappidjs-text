6c650be39e9b307deb98ed4bc1855b87c2b5cf58
Marcus Krejpowicz <krebbl@gmail.com>
2013-02-21 16:58:53 +0100

    cleaned up delete operation

--- text/operation/DeleteOperation.js
+++ text/operation/DeleteOperation.js
@@ -5,9 +5,9 @@ define(["text/operation/FlowOperation", "text/entity/FlowElement", "underscore",
             if (!(flowElement instanceof FlowElement)) {
                 throw new Error("Only elements of FlowElement allowed");
             }
-            this.$targetElement = flowElement;
             this.$textRange = textRange;
             this.$text = "";
+            this.callBase(flowElement);
         },
 
         doOperation: function () {
@@ -15,9 +15,6 @@ define(["text/operation/FlowOperation", "text/entity/FlowElement", "underscore",
                 absoluteStart = this.$textRange.$.absoluteStart,
                 absoluteEnd = this.$textRange.$.absoluteEnd,
                 lastElement;
-
-
-            var childIndex = element.findChildIndexAtPosition(absoluteStart);
 
             if (absoluteEnd > -1) {
                 lastElement = element.splitAtPosition(absoluteEnd);
@@ -28,12 +25,7 @@ define(["text/operation/FlowOperation", "text/entity/FlowElement", "underscore",
             }
 
             var startLeaf = element.getLastLeaf();
-//            if(startLeaf && startLeaf.textLength() === 0){
-//                var leafParent = startLeaf.$parent;
-//                if(leafParent.$parent){
-//                    leafParent.$parent.removeChild(leafParent);
-//                }
-//            }
+
             var newLeaf = lastElement.getFirstLeaf();
             if (newLeaf && this.$text) {
                 newLeaf.set('text', this.$text + newLeaf.$.text);
@@ -43,31 +35,29 @@ define(["text/operation/FlowOperation", "text/entity/FlowElement", "underscore",
                 element.addChild(child);
             });
 
-//            if(absoluteStart !== absoluteEnd){
-                var parent = startLeaf.$parent;
-                if (lastElement) {
-                    if (newLeaf && newLeaf !== startLeaf) {
-                        var endParent = newLeaf.$parent;
+            var parent = startLeaf.$parent;
+            if (lastElement) {
+                if (newLeaf && newLeaf !== startLeaf) {
+                    var endParent = newLeaf.$parent;
 
-                        var currentLeaf = newLeaf,
-                            nextLeaf;
-                        while (currentLeaf) {
-                            nextLeaf = currentLeaf.getNextLeaf(endParent);
-                            currentLeaf.$parent.removeChild(currentLeaf);
-                            if (currentLeaf.textLength() > 0) {
-                                parent.addChild(currentLeaf);
-                            }
-                            currentLeaf = nextLeaf;
+                    var currentLeaf = newLeaf,
+                        nextLeaf;
+                    while (currentLeaf) {
+                        nextLeaf = currentLeaf.getNextLeaf(endParent);
+                        currentLeaf.$parent.removeChild(currentLeaf);
+                        if (currentLeaf.textLength() > 0) {
+                            parent.addChild(currentLeaf);
                         }
-
-                        endParent.$parent && endParent.$parent.removeChild(endParent);
-
+                        currentLeaf = nextLeaf;
                     }
-//                }
+
+                    endParent.$parent && endParent.$parent.removeChild(endParent);
+
+                }
                 parent.mergeElements();
             }
 
-
+            this.callBase();
         }
 
 
